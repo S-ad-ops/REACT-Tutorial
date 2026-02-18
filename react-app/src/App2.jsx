7b1ddbc4b0242be4661836b83618151572a25809
soham-pawar <sohamrpawar.2018@gmail.com>
2026-02-18 13:21:44 +0000

    new2

---
+++ react-app/src/App2.jsx
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import In from "./in.css";
+
+function App2() {
+    return(
+        <>
+        <div className="app2">
+            <h1>Welcome to React</h1>
+            <Button />
+        </div>
+        </>
+    );
+}
+
+export default App2;
