1da5f0d1f762822099a2e7591287c349fe4667fc
soham-pawar <sohamrpawar.2018@gmail.com>
2026-02-24 17:10:46 +0000

    props

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,12 @@
-import Card from "./Card";
-
+import Student from "./Student";
 function App() {
   return (
     <>
-    <Card />
-    <Card />
+      <Student name="Alice" age ={20} isStudent={true}/>
+      <Student name="Patrik" age ={22} isStudent={false}/>
+      <Student name="Bob" age ={19} isStudent={true}/>
+      <Student name="Eve" age ={21} isStudent={false}/>
+      <Student name= "Charlie" />
     </>
   );
 }
